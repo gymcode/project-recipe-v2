9e41842244581113007af342607a8a6ade64153f
gymcode <kyleabs20@gmail.com>
2022-07-13 23:15:39 +0000

    tailwind css setup complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-red-300 h-20">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
