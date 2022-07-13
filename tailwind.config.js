0b05cc44a0306c53b3a1ed43822ca55c2412bce5
gymcode <kyleabs20@gmail.com>
2022-07-13 23:43:46 +0000

    removed some images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        "1/10": "93%",
+      },
+    },
+    screens: {
+      xs: "320px",
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
