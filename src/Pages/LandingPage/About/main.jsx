15141ac0f5d3d11a2002fc97d9e9cddb1f6ec7dd
gymcode <kyleabs20@gmail.com>
2022-07-19 00:34:26 +0000

    changes

--- src/Pages/LandingPage/About/main.jsx
+++ src/Pages/LandingPage/About/main.jsx
@@ -14,18 +14,18 @@ const AboutUsComponent = () => {
       id="about_section"
     >
       {/* for larger screens */}
-      <div className="hidden lg:grid md:grid grid-cols-5 gap-10 h-full">
+      <div className="hidden lg:grid grid-cols-5 gap-10 h-full">
         <div className="col-span-3 relative">
           <div
             data-aos="fade-down-right"
             data-aos-offset="300"
             data-aos-easing="ease-in-sine"
             data-aos-duration="1000"
-            className="absolute left-0 top-0"
+            className="lg:absolute left-0 top-0"
           >
             <motion.div
               whileHover={{ rotateY: 10, rotateY: -10, rotateZ: -10 }}
-              className="lg:w-[44rem] md:w-[30rem] lg:h-[28rem] md:h-[18rem] shadow-2xl bg-cover bg-center shadow-[#fad1c3]"
+              className="lg:w-[37vw] lg:h-[42vh] shadow-2xl bg-cover bg-center shadow-[#fad1c3]"
             >
               <div
                 className="w-full bg-cover bg-center h-full"
@@ -44,7 +44,7 @@ const AboutUsComponent = () => {
           >
             <motion.div
               whileHover={{ rotateY: -10, rotateY: 10, rotateZ: 10 }}
-              className="lg:w-[44rem] md:w-[30rem] lg:h-[28rem] md:h-[18rem] shadow-2xl shadow-[#fdd5c7]"
+              className="lg:w-[37vw] lg:h-[42vh] shadow-2xl shadow-[#fdd5c7]"
             >
               <div
                 className="w-full bg-cover bg-center h-full"
@@ -55,7 +55,7 @@ const AboutUsComponent = () => {
             </motion.div>
           </div>
         </div>
-        <div className="col-span-2 py-24">
+        <div className="lg:col-span-2 lg:py-28">
           <h2
             data-aos="zoom-in-down"
             data-aos-offset="300"
@@ -74,11 +74,20 @@ const AboutUsComponent = () => {
           >
             {AboutText.mid_section}
           </h3>
+          <p className="imprima-font text-[#959595] text-justify mt-12">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe
+            repellendus animi corrupti perspiciatis, id ratione exercitationem
+            odit nesciunt ipsum minus nihil. Quia expedita ad voluptatem quam
+            laboriosam repellat minus esse? Lorem ipsum dolor sit, amet
+            consectetur adipisicing elit. Cum tempora nam officia nisi
+            cupiditate illo accusamus quo voluptatem, odio fugiat. Quis
+            consequatur eligendi minus fuga soluta sunt illum laborum corrupti.
+          </p>
         </div>
       </div>
 
       {/* for lesser screens */}
-      <div className="block lg:hidden md:hidden">
+      <div className="block lg:hidden">
         <div className="">
           <h2 className="kreon-font text-2xl text-center">{AboutText.title}</h2>
           <h3 className="abel-font text-4xl mt-2 text-center">
@@ -87,7 +96,7 @@ const AboutUsComponent = () => {
         </div>
         <div className="mt-10">
           <div
-            className="w-full bg-cover bg-center h-60"
+            className="w-full bg-cover bg-center h-64 md:h-[26rem]"
             style={{
               backgroundImage: `url("https://ik.imagekit.io/yz8iaxzer/pexels-ready-made-3847709_Azn8bjB8C.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1658177206859")`,
             }}
