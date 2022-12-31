46ec5d8090f77c4a153a2dcc68fece77295b6799
gymcode <kyleabs20@gmail.com>
2022-12-31 12:56:51 +0000

    toast for ingredient selection

--- src/Components/RecipeInfoSubComponent/IngredientListing.js
+++ src/Components/RecipeInfoSubComponent/IngredientListing.js
@@ -1,6 +1,22 @@
+import { ERROR_TOAST, SUCCESS_TOAST } from 'Components';
 import React from 'react'
 
 const IngregientListing = ({itemArr}) => {
+  const [ingredient, setIngredient] = React.useState([])
+  console.log(ingredient)
+  function isSelectedCount (item) {
+    if (ingredient.includes(item)) {
+     return  ERROR_TOAST(`${item} has already been added. Try something else`) 
+    }
+
+    if (ingredient.length > 6) {
+      ERROR_TOAST("Cannot add more than 7 items") 
+    } else {
+      setIngredient([...ingredient, item])
+      SUCCESS_TOAST(`${item} has been added for nutritional analysis`)
+    }
+  }
+
   return (
     <>
       {itemArr.map((item, index) => (
@@ -11,7 +27,7 @@ const IngregientListing = ({itemArr}) => {
             </div>
             <p className="pl-6 xl:pl-4 col-span-11 imprima-font">
               {item.name}
-              <span className="text-[#818181]"> ({item.original})</span>
+              <span onClick={()=> isSelectedCount(item.original)} className="text-[#818181] cursor-pointer hover:text-[#F84605]">({item.original})</span>
             </p>
           </div>
         </>
